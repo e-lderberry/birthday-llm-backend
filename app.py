f8e1c5fec4e47ba87e65c26ec4fee8b5f66643e0
e-lderberry <shamardina.tatiana@gmail.com>
2026-01-30 22:56:06 +0300

    add cors

--- app.py
+++ app.py
@@ -1,8 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from llama_cpp import Llama
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "https://e-lderberry.github.io"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 # Load model ONCE at startup
 llm = Llama(
